dca3f56c47ff5a56d479bd7636ab6b014c925776
paulsammut <paul.c.sammut@gmail.com>
2016-11-22 11:24:49 -0500

    stuck the code in the main function

--- learn_cpp/extra0/ex01/main.cpp
+++ learn_cpp/extra0/ex01/main.cpp
@@ -13,23 +13,28 @@ using namespace std;
 class TMyClass
 {
 public:
-    int DoIt    (float a, char b, char c) 
-    {  
-       cout << "TMyClass::DoIt" << std::endl;
-       return a+b+c;   
+    int DoIt    (float a, char b, char c)
+    {
+        cout << "TMyClass::DoIt" << std::endl;
+        return a+b+c;
     }
 
-    int DoMore  (float a, char b, char c) const  
-    {   
+    int DoMore  (float a, char b, char c) const
+    {
         cout << "TMyClass::DoMore" << std::endl;
-        return a+b+c;   
+        return a+b+c;
     }
 };
 
-int (*pt2Function)(float, char, char) = NULL;                       // C
-int (TMyClass::*pt2Member)(float, char, char) = NULL;               // C++
-int (TMyClass::*pt2ConstMember)(float, char, char) const = NULL;    // C++
+int main()
+{
+    int (*pt2Function)(float, char, char) = NULL;                       // C
+    int (TMyClass::*pt2Member)(float, char, char) = NULL;               // C++
+    int (TMyClass::*pt2ConstMember)(float, char, char) const = NULL;    // C++
 
 
-pt2ConstMember = &TMyClass::DoMore; // correct assignment using the address operator
-pt2Member = &TMyClass::DoIt;    // note: <pt2Member> may also legally point to &DoMore
+    pt2ConstMember = &TMyClass::DoMore; // correct assignment using the address operator
+    pt2Member = &TMyClass::DoIt;    // note: <pt2Member> may also legally point to &DoMore
+
+    cout << "pt2Member: " << pt2Member << std::endl; 
+}
