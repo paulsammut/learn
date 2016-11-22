384b61597e9cbe9be2dd7446a0189848ca03b48d
paulsammut <paul.c.sammut@gmail.com>
2016-11-22 11:13:16 -0500

    trying to learn function pointers

---
+++ learn_cpp/extra0/ex00/main.cpp
@@ -0,0 +1,56 @@
+// Funciton Pointer Extra Lesson!
+// http://www.newty.de/fpt/intro.html
+// Lesson 1.
+//
+//------------------------------------------------------------------------------------
+// 1.2 Introductory Example or How to Replace a Switch-Statement
+// Task: Perform one of the four basic arithmetic operations specified by the
+//       characters '+', '-', '*' or '/'.
+
+#include <cassert>
+#include <iostream>
+
+
+// The four arithmetic operations ... one of these functions is selected
+// at runtime with a swicth or a function pointer
+float Plus    (float a, float b) { return a+b; }
+float Minus   (float a, float b) { return a-b; }
+float Multiply(float a, float b) { return a*b; } 
+float Divide  (float a, float b) { return a/b; }
+
+// Solution with a switch statement - <opCode> specifies which oeration to execute
+void Switch(float a, float b, char opCode)
+{
+    float result;
+
+    // execute operation
+    switch(opCode)
+    {
+        case '+' : result = Plus        (a,b); break;
+        case '-' : result = Minus       (a,b); break;
+        case '*' : result = Multiply    (a,b); break;
+        case '/' : result = Divide      (a,b); break;
+    }
+
+    std::cout << "Switch: 2+5=" << result << std::endl;      //display result
+}
+
+// Solution with a function pointer - <pt2Func> is a function pointer and points to 
+// a function which takes two floats and returns a float. The function pointer 
+// "specifies" which operation shall be executed.
+void Switch_With_Function_Pointer(float a, float b, float (*pt2Func)(float, float))
+{
+    float result = pt2Func(a, b);   //call using function pointer
+
+    std::cout << "Switch replaced by function pointer: 2-5="; // display result
+    std::cout << result << std::endl;
+}
+
+int main()
+{
+   std::cout << std::endl << "Executing function 'Replace_A_Switch'" << std::endl;
+
+   Switch(2, 5, /* '+' specifies function 'Plus' to be executed */ '+');
+   Switch_With_Function_Pointer(2, 5, /* pointer to function 'Minus' */ &Minus);
+}
+
---
+++ learn_cpp/extra0/ex01/main.cpp
@@ -0,0 +1,35 @@
+// Funciton Pointer Extra Lesson!
+// http://www.newty.de/fpt/intro.html
+// Lesson 2.
+//
+//
+
+// 2.1 define a funciton pointer and initialize to NULL
+
+#include <iostream>
+
+using namespace std;
+
+class TMyClass
+{
+public:
+    int DoIt    (float a, char b, char c) 
+    {  
+       cout << "TMyClass::DoIt" << std::endl;
+       return a+b+c;   
+    }
+
+    int DoMore  (float a, char b, char c) const  
+    {   
+        cout << "TMyClass::DoMore" << std::endl;
+        return a+b+c;   
+    }
+};
+
+int (*pt2Function)(float, char, char) = NULL;                       // C
+int (TMyClass::*pt2Member)(float, char, char) = NULL;               // C++
+int (TMyClass::*pt2ConstMember)(float, char, char) const = NULL;    // C++
+
+
+pt2ConstMember = &TMyClass::DoMore; // correct assignment using the address operator
+pt2Member = &TMyClass::DoIt;    // note: <pt2Member> may also legally point to &DoMore
