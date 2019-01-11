37fef782166ab3c71f6a32276085e4a0b5fb448e
paulsammut <paul.c.sammut@gmail.com>
2019-01-11 17:47:52 -0500

    trying out a new way

--- learn_algo/length_of_longest_substring/main.cpp
+++ learn_algo/length_of_longest_substring/main.cpp
@@ -6,25 +6,24 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int run = 0;    // running counter
+        int i = 0, j=0;    // running counter
         int max = 0;    // longest run ever
-        bool charArray[256] = {false};  /// our "key" array. Keeps track of any previously used characters
+        bool charA[256] = {false};  /// our "key" array. Keeps track of any previously used characters
         for(int j = 0; j < s.length(); j++)
         {
-            std::fill(charArray, charArray+255, 0);
-            run = 0;
-            // Iterate through chars in string
-            for(int i = j; i<s.length(); i++)
-            { 
-                // if we have a recurring character reset our array
-                if(charArray[s[i]]) {
-                    std::fill(charArray, charArray+255, 0);
-                    run = 0;
-                }
-
-                charArray[s[i]] = true;
-                run++;
-                max = (run>max) ? run : max;
+            // if we haven't yet seen the character, update our max run
+            if(!charA[s[j]])
+            {
+                max = (max > (j+1-i)) ? max : (j+1-i);
+                charA[s[j]] = true;
+            }
+            // if we have, then increase our sliding window i size and remove
+            // that character
+            else
+            {
+                charA[s[i]] = false;
+                i++; 
+                j--;
             }
         }
         return max;
@@ -37,4 +36,3 @@ int main(void)
     std::cout << "Hi!" << solution.lengthOfLongestSubstring("asjrgapa") << std::endl;
     return 0;
 }
-
